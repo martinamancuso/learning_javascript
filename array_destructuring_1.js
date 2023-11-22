cf03f809b43590ca3b4838e7ce0a7567fd0c7b8a
martinamancuso <martinamancuso24@gmail.com>
2023-11-22 10:14:04 +0100

    array destructuring excercise

---
+++ array_destructuring_1.js
@@ -0,0 +1,12 @@
+/*
+Dato un array di colori con tre elementi, 
+utilizza la destrutturazione dell'array per creare variabili 
+firstColor, secondColor e thirdColor che contengono i valori dell'array.
+*/
+
+const colors = ["red", "green", "blue"];
+
+const [firstColor, secondColor, thirdColor] = colors;
+console.log(firstColor);
+console.log(secondColor);
+console.log(thirdColor);

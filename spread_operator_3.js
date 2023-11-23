adf18d68c876d687d494b0df0a618942469071da
martinamancuso <martinamancuso24@gmail.com>
2023-11-23 12:34:14 +0100

    spread operator exercise #3

--- spread_operator_3.js
+++ spread_operator_3.js
@@ -5,3 +5,8 @@ che sia una copia superficiale di 'originalArray' utilizzando l'operatore spread
 */
 
 const originalArray = [1, 2, 3, 4, 5];
+
+const cloneArray = [...originalArray];
+
+console.log('Original Array:', originalArray);
+console.log('Clone Array:', cloneArray);

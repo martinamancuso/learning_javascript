c5f0b25ee0b4e5a5e3b12e8ed3976471825446f4
martinamancuso <martinamancuso24@gmail.com>
2023-12-04 21:44:35 +0100

    promise exercise #5

---
+++ promise/promise_5.js
@@ -0,0 +1,31 @@
+// Convertire la seguente funzione basata su callback in una funzione basata su Promise.
+
+function callback_BasedFunction(arg1, arg2, callback) {
+  setTimeout(() => {
+    const result = arg1 + arg2;
+    if (result % 2 !== 0) {
+      callback(null, result);
+    } else {
+      callback(new Error('Result is not odd!'), null);
+    }
+  }, 1000);
+};
+
+function promise_BasedFunction(arg1, arg2) {
+  return new Promise((resolve, reject) => {
+    const result = arg1 + arg2;
+    setTimeout(() => {
+      if (result % 2 !== 0) {
+        resolve(result);
+      } else {
+        reject(new Error('Result is not odd!'));
+      }
+    }, 1000);
+  });
+}
+
+promise_BasedFunction(2, 4).then((data) => {
+  console.log(data);
+}).catch((error) => {
+  console.log(error);
+});

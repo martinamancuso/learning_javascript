b1871273c258a5d0f1c8c042667d1ecf11976241
martinamancuso <martinamancuso24@gmail.com>
2023-12-05 12:25:51 +0100

    NB added to the promise exercise #4

--- promise/promise_4.js
+++ promise/promise_4.js
@@ -8,6 +8,24 @@ a volte fallirà con un messaggio di errore.
 
 - La chiamata avrà successo con il messaggio "Data retrieved successfully", se il valore random è inferiore a 0.5, 
 altrimenti fallisce con il messaggio "Error: Failed to fetch data".
+
+
+NB:
+let success = Math.random() * 0.5;
+La variabile 'success' viene generata come un numero casuale tra 0 e 0.5. 
+
+Questo significa che il valore di 'success' sarà sempre inferiore a 0.5, 
+quindi la Promise si risolverà sempre con successo e non fallirà mai.
+
+Per far sì che la Promise possa anche fallire, 
+si dovrebbe generare un numero casuale tra 0 e 1. 
+
+let success = Math.random();
+In quanto il metodo 'math.random', appunto, genera un numero casuale tra 0 e 1.
+
+In questo modo, ci sarà una probabilità del 50% che 
+il numero generato sia inferiore a 0.5 (la richiesta avrà successo) 
+o superiore a 0.5 (la richiesta fallirà).
 */
 
 function fetchDataFromAPI() {
